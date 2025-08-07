b30558abd7e8e49f5c1612f95a9254420bf6b03e
Labici Ioan <ioanlabici@gmail.com>
2025-08-07 08:38:43 +0300

    lam-1: Authetication logic.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('auth')->group(function() {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
+
